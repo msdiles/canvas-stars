5bd3f22ca2f2fffa7a791258b10e9438273ed3dc
Dmitrii Leshchuk <msdiles@gmail.com>
2020-09-30 18:02:47 +0300

    v 0.0.2

--- src/canvas.js
+++ src/canvas.js
@@ -1,5 +1,5 @@
-import { animationFrameScheduler, from, fromEvent, interval, of, timer } from "rxjs"
-import { audit, debounce, debounceTime, map, mergeAll, tap, toArray } from "rxjs/operators"
+import { fromEvent } from "rxjs"
+import { map } from "rxjs/operators"
 import Stars from "../stars"
 
 class Canvas {
@@ -10,13 +10,16 @@ class Canvas {
     this.ctx = this.canvas.getContext("2d")
     this.setBasicConf()
 
-    this.units = []
+    this.mousePosition = { x: 0, y: 0 }
+
     this.circle = {
       offsetWidth: 40,
       offsetHeight: 40,
       radius: 4,
       forceVector: { x: 3, y: 1 },
     }
+    this.drawFunctions = []
+    this.arrayOfConnectedUnits = []
   }
 
   createUnits(n) {
@@ -24,31 +27,31 @@ class Canvas {
   }
 
   drawUnits() {
-    const canvas = this
-    const scheduler = animationFrameScheduler.schedule(
-      function (units) {
-        of(units)
-          .pipe(
-            tap(() => {
-              canvas.clearCanvas()
-              canvas.setBasicConf()
-            }),
-            mergeAll(),
-            tap((unit) => {
-              canvas.stars.drawUnit(canvas, unit)
-            }),
-            map((unit) => canvas.stars.unitNextFrame(unit)),
-            toArray()
-          )
-          .subscribe((x) => scheduler.schedule(x))
-      },
-      0,
-      canvas.stars.units
-    )
+    this.clearCanvas()
+    this.setBasicConf()
+    this.stars.units = this.stars.units.map((unit) => {
+      this.stars.drawUnit(this, unit)
+      const newUnit = this.stars.unitNextFrame(unit)
+      return newUnit
+    })
+  }
+
+  addObjectsToDraw(func) {
+    this.drawFunctions.push(func)
+  }
+
+  draw() {
+    const update = () => {
+      this.drawFunctions.forEach((func) => {
+        func.call(this)
+      })
+      requestAnimationFrame(update)
+    }
+
+    window.requestAnimationFrame(update)
   }
 
   checkMouse() {
-    const thisCanvas = this
     fromEvent(this.canvas, "mousemove")
       .pipe(
         map((e) => {
@@ -57,33 +60,37 @@ class Canvas {
             x: e.clientX - rect.left,
             y: e.clientY - rect.top,
           }
-        }),
-        audit(ev => interval(1000)),
-
+        })
       )
       .subscribe((e) => {
-        console.log(e)
-        // this.stars.units.forEach((unit) => {
-        //   if (Math.abs(e.x - unit.X) < 50 && Math.abs(e.y - unit.Y) < 50) {
-        //     this.drawLines(e, unit)
-        //     // const length = +Math.sqrt(
-        //     //   +Math.pow(+Math.abs(e.x - unit.X).toFixed(1), 2).toFixed(1) +
-        //     //     +Math.pow(+Math.abs(e.y - unit.Y).toFixed(1), 2).toFixed(1)
-        //     // ).toFixed(1)
-        //     // console.log(length,unit);
-        //     // connections.set(length, unit)
-        //   }
-        // })
-        // console.log(connections);
+        this.mousePosition = e
       })
+    setInterval(() => {
+      this.arrayOfConnectedUnits = []
+      this.stars.units.forEach((unit) => {
+        if (
+          Math.abs(this.mousePosition.x - unit.X) < 100 &&
+          Math.abs(this.mousePosition.y - unit.Y) < 100
+        ) {
+          const vectorLength = Math.sqrt(
+            (this.mousePosition.x - unit.X) ** 2 +
+              (this.mousePosition.y - unit.Y) ** 2
+          )
+          this.arrayOfConnectedUnits.push({ ...unit, vectforLength })
+        }
+      })
+    }, 1000 / 60)
   }
 
-  drawLines(center, unit) {
-    console.log(center.x, center.y, unit)
-    this.ctx.beginPath()
-    this.ctx.moveTo(100, 100)
-    this.ctx.lineTo(100, 100)
-    this.ctx.stroke()
+  drawLines() {
+    this.arrayOfConnectedUnits.forEach((unit, idx) => {
+      this.ctx.beginPath()
+      this.ctx.moveTo(this.mousePosition.x, this.mousePosition.y)
+      this.ctx.lineTo(unit.X, unit.Y)
+      this.ctx.strokeStyle = `rgba(80,210,220,${1 / (unit.vectorLength / 50)})`
+      this.ctx.lineWidth = 1 / (unit.vectorLength / 50)
+      this.ctx.stroke()
+    })
   }
 
   setBasicConf() {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import Canvas from "./canvas.js"
 const canvasEl = document.getElementById("canvas")
 
 const canvas = new Canvas(canvasEl)
-canvas.createUnits(10)
-canvas.drawUnits()
+canvas.createUnits(100)
+canvas.addObjectsToDraw(canvas.drawUnits)
+canvas.addObjectsToDraw(canvas.drawLines)
 canvas.checkMouse()
+
+canvas.draw()
